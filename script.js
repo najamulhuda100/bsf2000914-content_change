c0cc552fcb6e6cf115c6e7da9904da1b6b3cc725
najamulhuda100 <114976839+najamulhuda100@users.noreply.github.com>
2022-10-04 22:55:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let hellobtn = document.querySelector('button');
+// hellobtn.addEventListener('click', inputmsg);
+
+// function inputmsg(){
+//     let name = prompt('Enter name of Student');
+//     hellobtn.textContent = 'Roll no. 1:' + name;
+// }
+
+let hellobtn = document.querySelector('button');
+hellobtn.addEventListener('click', inputmsg);
+
+function inputmsg(){
+    let name = prompt('Enter name of Student');
+    hellobtn.textContent = 'Roll no. 1:' + name;
+}
+
+// Boolean variable
+
+var YES = true;
+var NO = false;
+if (YES)
+{
+    alert("This code block will be executed")
+}
+if (NO)
+{
+    alert("This code block will not be executed")
+}
+
+// Undefined Variable
+
+var myVar;
+alert(myVar);
+
+// String Variable
+
+var str1 = "Hello World";
+var str2 = 'someone';
+
+// Null Variable
+
+var myVar = null;
+alert(myVar);
+
+// Number variable
+
+var float = 1.5;
+var int = 15;
+var char = 'M';
+var exponential = 2.3e2;
+
+
+
+
